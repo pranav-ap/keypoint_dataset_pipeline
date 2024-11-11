dfbab2364508ab44d577252d541001432f347286
pranav <pranavap96@gmail.com>
2024-11-11 23:15:10 +0100

    refactor

--- pipeline/DataStore.py
+++ pipeline/DataStore.py
@@ -24,9 +24,7 @@ class DataStore:
         self._detector = self._file.create_group(f'{config.task.cam}/detector')
         self._matcher = self._file.create_group(f'{config.task.cam}/matcher')
         self._filter = self._file.create_group(f'{config.task.cam}/filter')
-
-        # Create groups in the results file
-        self._results_matches = self._file.create_group(f'{config.task.cam}/matches')
+        self._matches = self._file.create_group(f'{config.task.cam}/matches')
 
         # Setup 'detector' subgroups
         self.detector_image_level_normalised = self._detector.create_group('image_level/normalised')
@@ -47,18 +45,16 @@ class DataStore:
         self.filter_patch_level_which_patch = self._filter.create_group('patch_level/which_patch')
 
         # Setup results subgroups
-        self.results_original_reference_coords = self._results_matches.create_group('original/reference_coords')
-        self.results_original_target_coords = self._results_matches.create_group('original/target_coords')
-        self.results_small_reference_coords = self._results_matches.create_group('small/reference_coords')
-        self.results_small_target_coords = self._results_matches.create_group('small/target_coords')
+        self.results_original_reference_coords = self._matches.create_group('original/reference_coords')
+        self.results_original_target_coords = self._matches.create_group('original/target_coords')
+        self.results_small_reference_coords = self._matches.create_group('small/reference_coords')
+        self.results_small_target_coords = self._matches.create_group('small/target_coords')
 
     def _init_groups_read_mode(self):
         self._detector = self._file[f'{config.task.cam}/detector']
         self._matcher = self._file[f'{config.task.cam}/matcher']
         self._filter = self._file[f'{config.task.cam}/filter']
-
-        # Create groups in the results file
-        self._results_matches = self._file[f'{config.task.cam}/matches']
+        self._matches = self._file[f'{config.task.cam}/matches']
 
         # Setup 'detector' subgroups
         self.detector_image_level_normalised = self._detector['image_level/normalised']
@@ -79,10 +75,10 @@ class DataStore:
         self.filter_patch_level_which_patch = self._filter['patch_level/which_patch']
 
         # Setup results subgroups
-        self.results_original_reference_coords = self._results_matches['original/reference_coords']
-        self.results_original_target_coords = self._results_matches['original/target_coords']
-        self.results_small_reference_coords = self._results_matches['small/reference_coords']
-        self.results_small_target_coords = self._results_matches['small/target_coords']
+        self.results_original_reference_coords = self._matches['original/reference_coords']
+        self.results_original_target_coords = self._matches['original/target_coords']
+        self.results_small_reference_coords = self._matches['small/reference_coords']
+        self.results_small_target_coords = self._matches['small/target_coords']
 
     def get_random_pair(self):
         keys = list(self.results_small_reference_coords.keys())
