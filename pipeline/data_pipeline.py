d8ee0495b505d071c99d526b2c364cfe74a98950
pranav <pranavap96@gmail.com>
2024-11-11 09:38:13 +0100

    add imu filter

--- pipeline/data_pipeline.py
+++ pipeline/data_pipeline.py
@@ -7,6 +7,7 @@ from config import config
 from utils import get_best_device, logger, make_clear_directory
 from .DataFilter import DataFilter
 from .DataStore import DataStore
+from .IMUFilter import IMUFilter
 from .detectors import DeDoDeDetector
 from .matchers import RoMaMatcher
 
@@ -33,16 +34,18 @@ class DataPipeline:
         os.chdir(config.paths.roots.project)
         self.data_filter = DataFilter(self.data_store)
 
+        self.imu_filter = IMUFilter()
+
     @staticmethod
     def get_image_names():
-        if config.task.name == 'samples' or config.task.consider_samples:
+        if config.task.consider_samples:
             return [
                 str(config.samples[config.task.name].reference),
                 str(config.samples[config.task.name].target)
             ]
 
-        df = pd.read_csv(config.paths[config.task.name].csv)
-        df['timestamp'] = pd.to_datetime(df['#timestamp [ns]'], unit='ns')
+        df = pd.read_csv(config.paths[config.task.name].images_csv, header=0, names=('timestamp', 'filename'))
+        df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ns')
         df = df.sort_values(by='timestamp')
         df['filename'] = df['filename'].str.replace(".png", "", regex=False)
         image_names = df['filename'].tolist()
@@ -53,19 +56,12 @@ class DataPipeline:
 
         return image_names
 
-    def _run(self):
+    def _process_images(self):
         self.data_store.init()
 
-        os.chdir(config.paths.roots.project)
         image_names = self.get_image_names()
-
-        os.chdir(f'{config.paths.roots.project}/libs/DeDoDe')
         self.detector.extract_keypoints(image_names)
-
-        os.chdir(f'{config.paths.roots.project}/libs/RoMa')
         self.matcher.extract_warp_certainty(image_names)
-
-        os.chdir(config.paths.roots.project)
         self.data_filter.extract_good_matches(image_names)
 
     def run(self):
@@ -77,12 +73,16 @@ class DataPipeline:
             OmegaConf.save(config, config_filename)
 
             if config.task.consider_samples:
-                self._run()
+                self._process_images()
             else:
+                # Process IMU
+                self.imu_filter.process_imu()
+
+                # Process Images
                 for cam in ['cam0', 'cam1']:
                     config.task.cam = cam
                     logger.info(f'Camera {cam}')
-                    self._run()
+                    self._process_images()
 
         finally:
             self.data_store.close()
