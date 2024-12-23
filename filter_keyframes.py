7080f667140cf07abfd6f98d350960674c315165
Pranav <pranavap96@gmail.com>
2024-12-23 12:17:05 +0100

    refactor

--- filter_keyframes.py
+++ filter_keyframes.py
@@ -1,11 +1,12 @@
 import json
+import shutil
 
 import numpy as np
 import pandas as pd
 from scipy.spatial.transform import Rotation as R
 from pathlib import Path
 from config import config
-from utils import logger
+from utils import logger, make_clear_directory
 
 
 def to_transformation_matrix(d):
@@ -27,8 +28,10 @@ def read_calib_json():
     # calib_path = r"D:/thesis_code/datasets/monado_slam/M_monado_datasets_MO_odyssey_plus_extras_calibration.json"
     with open(calib_path, 'r') as file:
         data = json.load(file)
-        data = data['value0']['T_imu_cam']
-        return data
+
+    T_list = data['value0']['T_imu_cam']
+    intrinsics_list = data['value0']['intrinsics']
+    return T_list, intrinsics_list
 
 
 def read_imu_csv():
@@ -56,14 +59,15 @@ def read_images_csv():
     files = pd.read_csv(files_path, header=0, names=('timestamp', 'filename'))
     files['ts'] = pd.to_datetime(files['timestamp'], unit='ns')
     files = files.sort_values(by='ts').reset_index(drop=True)
-    logger.info(f"files.shape : {files.shape}")
+    logger.info(f"Original files.shape : {files.shape}")
 
     def image_exists(filename):
         image_path = Path(f"{config.paths[config.task.name].images}/{filename.strip()}")
         return image_path.exists()
 
     filtered_files = files[files['filename'].apply(image_exists)].reset_index(drop=True)
-    logger.info(f"filtered_files.shape : {filtered_files.shape}")
+    if filtered_files.shape[0] < files.shape[0]:
+        logger.debug(f"After Image Exists Check files.shape : {filtered_files.shape}")
 
     return filtered_files
 
@@ -75,15 +79,19 @@ def align_rows(files, gt):
         direction="backward",
     ).dropna()
 
-    aligned_df.to_csv(config.paths.basalt.aligned_csv, index=False, header=True)
-    logger.info(f"aligned_df.shape : {aligned_df.shape}")
+    # aligned_df.to_csv(config.paths.basalt.aligned_csv, index=False, header=True)
+    # logger.debug(f"aligned_df.shape : {aligned_df.shape}")
 
     return aligned_df
 
 
-def filter_rows(aligned_df, T_i_c0):
-    displacement_threshold = 0.02  # meters - 2 cm
-    angle_threshold = 20 # 10  # degrees
+def filter_rows(aligned_df, T_i_c, intrinsics):
+    # csv in meters
+    # 20 cm to 0.2 m
+    displacement_threshold =config.task.displacement_threshold / 100  
+    # csv in radians
+    # 20 degrees to 0.34 rad
+    angle_threshold = config.task.angle_threshold * np.pi / 180  
 
     T_w_i = None
     T_w_c_t1 = None
@@ -93,19 +101,19 @@ def filter_rows(aligned_df, T_i_c0):
     for index, entry in aligned_df.iterrows():
         if T_w_i is None:
             T_w_i = to_transformation_matrix(entry)
-            T_w_c_t1 = T_w_i @ T_i_c0
+            T_w_c_t1 = T_w_i @ T_i_c
             keyframes_indices.append(index)
             continue
 
         T_w_i = to_transformation_matrix(entry)
-        T_w_c_t2 = T_w_i @ T_i_c0
+        T_w_c_t2 = T_w_i @ T_i_c
 
         R_t1 = T_w_c_t1[:3, :3]
         R_t2 = T_w_c_t2[:3, :3]
 
         R_relative = np.linalg.inv(R_t1) @ R_t2
-        euler_angles = R.from_matrix(R_relative).as_euler('xyz', degrees=True)
-        angle_norm = np.linalg.norm(euler_angles)
+        axis_angles = R.from_matrix(R_relative).as_rotvec()
+        angle_norm = np.linalg.norm(axis_angles)
 
         if angle_norm > angle_threshold:
             keyframes_indices.append(index)
@@ -119,13 +127,127 @@ def filter_rows(aligned_df, T_i_c0):
         if displacement > displacement_threshold:
             keyframes_indices.append(index)
             T_w_c_t1 = T_w_c_t2
+       
 
     keyframes_df = aligned_df.iloc[keyframes_indices].reset_index(drop=True)
     keyframes_df = keyframes_df.drop(columns=['ts_x', 'ts_y'])
     keyframes_df.to_csv(config.paths.basalt.keyframes_csv, index=False, header=True)
 
-    # files_path = r"D:\thesis_code\datasets\monado_slam\MOO07_mapping_easy\mav0\cam0\data_filtered.csv"
-    # keyframes_df.to_csv(files_path, index=False, header=True)
+    if config.task.output_folder_name == 'output_filtered_test':
+        image_names = keyframes_df['filename'].tolist()
+
+        for name in image_names:
+            image_path_from = Path(f"{config.paths[config.task.name].images}/{name.strip()}")
+            image_path_to = Path(f"{config.paths[config.task.name].output_cam}/images/{name.strip()}")
+            shutil.copy(image_path_from, image_path_to)
+
+    logger.info(f"keyframes_df.shape : {keyframes_df.shape}")
+
+
+def filter_blurred_rows(aligned_df, T_i_c, intrinsics):
+    blur_threshold = config.task.blur_threshold  
+
+    T_w_i = None
+    T_w_c_t1 = None
+
+    keyframes_indices = []
+    
+    for i in range(len(df) - 1):
+        row1, row2 = df.iloc[i], df.iloc[i + 1]
+        
+        T_w_i = to_transformation_matrix(entry)
+        T_w_c_t1 = T_w_i @ T_i_c
+
+        T_w_i = to_transformation_matrix(entry)
+        T_w_c_t2 = T_w_i @ T_i_c
+
+        scaled_z = 2 * np.array([0, 0, 1, 1]) # added 1 for homogenous T
+        V = np.linalg.inv(T_w_c_t2) @ T_w_c_t1 @ scaled_z 
+
+        x, y, z = V[0], V[1], V[2]
+
+        fx, fy = intrinsics['fx'], intrinsics['fy']
+        cx, cy = intrinsics['cx'], intrinsics['cy']
+
+        a = np.array([
+            config.image.crop_image_shape[0] / 2, 
+            config.image.crop_image_shape[1] / 2
+        ])
+
+        b = np.array([
+            (fx * x / z) + cx, 
+            (fy * y / z) + cy,
+        ])
+
+        result = np.linalg.norm(a - b)
+
+        if result > blur_threshold:
+            keyframes_indices.append(index)
+            T_w_c_t1 = T_w_c_t2
+            continue
+     
+    keyframes_df = aligned_df.iloc[keyframes_indices].reset_index(drop=True)
+    keyframes_df = keyframes_df.drop(columns=['ts_x', 'ts_y'])
+    keyframes_df.to_csv(config.paths.basalt.keyframes_csv, index=False, header=True)
+
+    if config.task.output_folder_name == 'output_filtered_test':
+        image_names = keyframes_df['filename'].tolist()
+
+        for name in image_names:
+            image_path_from = Path(f"{config.paths[config.task.name].images}/{name.strip()}")
+            image_path_to = Path(f"{config.paths[config.task.name].output_cam}/images/{name.strip()}")
+            shutil.copy(image_path_from, image_path_to)
+
+    logger.info(f"keyframes_df.shape : {keyframes_df.shape}")
+
+
+        
+    for index, entry in aligned_df.iterrows():
+        if T_w_i is None:
+            T_w_i = to_transformation_matrix(entry)
+            T_w_c_t1 = T_w_i @ T_i_c
+            keyframes_indices.append(index)
+            continue
+
+        T_w_i = to_transformation_matrix(entry)
+        T_w_c_t2 = T_w_i @ T_i_c
+
+        scaled_z = 2 * np.array([0, 0, 1, 1]) # added 1 for homogenous T
+        V = np.linalg.inv(T_w_c_t2) @ T_w_c_t1 @ scaled_z 
+
+        x, y, z = V[0], V[1], V[2]
+
+        fx, fy = intrinsics['fx'], intrinsics['fy']
+        cx, cy = intrinsics['cx'], intrinsics['cy']
+
+        a = np.array([
+            config.image.crop_image_shape[0] / 2, 
+            config.image.crop_image_shape[1] / 2
+        ])
+
+        b = np.array([
+            (fx * x / z) + cx, 
+            (fy * y / z) + cy,
+        ])
+
+        result = np.linalg.norm(a - b)
+
+        if result > blur_threshold:
+            keyframes_indices.append(index)
+            T_w_c_t1 = T_w_c_t2
+            continue
+     
+    keyframes_df = aligned_df.iloc[keyframes_indices].reset_index(drop=True)
+    keyframes_df = keyframes_df.drop(columns=['ts_x', 'ts_y'])
+    keyframes_df.to_csv(config.paths.basalt.keyframes_csv, index=False, header=True)
+
+    if config.task.output_folder_name == 'output_filtered_test':
+        image_names = keyframes_df['filename'].tolist()
+
+        for name in image_names:
+            image_path_from = Path(f"{config.paths[config.task.name].images}/{name.strip()}")
+            image_path_to = Path(f"{config.paths[config.task.name].output_cam}/images/{name.strip()}")
+            shutil.copy(image_path_from, image_path_to)
 
     logger.info(f"keyframes_df.shape : {keyframes_df.shape}")
 
@@ -139,20 +261,29 @@ def align_and_filter_rows():
 
         gt = read_gt_csv()
 
-        calibs = read_calib_json()
+        T_list, intrinsics_list = read_calib_json()
 
-        for index, calib in enumerate(calibs):
+        for index, (T, intrinsics) in enumerate(zip(T_list, intrinsics_list)):
             if index == 2:
                 break
-
+                
             config.task.cam = f'cam{index}'
             logger.info(f'cam{index}')
 
-            T_i_c = to_transformation_matrix(calib)
+            make_clear_directory(config.paths.basalt.output_cam)
+            make_clear_directory(f'{config.paths.basalt.output_cam}/images')
+
+            T_i_c = to_transformation_matrix(T)
+
+            intrinsics = intrinsics['intrinsics']
 
             filenames = read_images_csv()
             aligned_df = align_rows(filenames, gt)
-            filter_rows(aligned_df, T_i_c)
+
+            if config.task.use_blur_filter:
+                filter_blurred_rows(aligned_df, T_i_c, intrinsics)
+            else:
+                filter_rows(aligned_df, T_i_c, intrinsics)
 
 
 def main():
